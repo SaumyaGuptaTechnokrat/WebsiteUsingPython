2bd0e0fc36de4073f163f2b505e96028518ae414
SaumyaGuptaTechnokrat <65768914+SaumyaGuptaTechnokrat@users.noreply.github.com>
2023-09-06 02:55:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
-from streamlit_lottie import st_lottie
 st.set_page_config(page_title="Cool Wesbite | Python",page_icon=":tada:",layout="wide")
 # def load_lottieurl(url):
+#fhfd
 #     r=requests.get(url)
 #     if r.status_code !=200:
 #         return None
